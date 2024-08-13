be68acdbadeb6a46f629f8684772b3aab77763d5
pavlobarabah <pavlobarabah@gmail.com>
2024-08-13 14:44:03 +0300

    started work on tech task

---
+++ src/test/java/com/example/javatraineetechtask/JavaTraineeTechTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javatraineetechtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTraineeTechTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
